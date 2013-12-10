b6f09b0ccce077f9e45b1b23ccbb00337691add5
alejandrosuarezocampo <alejandrosuarezocampo@192.168.1.134>
2013-12-10 18:17:07 +0000

    Practica 1 de la asignatura PR3. Las clases y metodos que faltan fueron suministrados por aulaga, el servidor de docencia de la ulpgc.

---
+++ src/practica1pr3/DatosEstadisticos.java
@@ -0,0 +1,22 @@
+package practica1pr3;
+
+public class DatosEstadisticos {
+    
+    private float time;
+    
+    public DatosEstadisticos() {
+    }
+    
+    public void añadeTiempo(float f) {
+        time += f;
+    }
+    
+    public void estableceTiempo(float f)  {
+        time = f;
+    }
+    
+    public float dameTiempo() {
+        return time;
+    }
+
+}
